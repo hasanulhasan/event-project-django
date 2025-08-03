5481d03d47e72030be9ab1f27c263f4a8dc2da10
hasanulhasan <hasanlink71@yahoo.com>
2025-08-03 20:00:20 +0600

    models and data added

--- event/settings.py
+++ event/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'event.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'event',
+        'USER': 'postgres',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
--- events/models.py
+++ events/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='events')
+
+    def __str__(self):
+        return f"{self.name} on {self.date}"
+
+
+class Participant(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    events = models.ManyToManyField(Event, related_name='participants')
+
+    def __str__(self):
+        return self.name
+
